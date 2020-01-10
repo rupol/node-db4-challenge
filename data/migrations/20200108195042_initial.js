0d4e678683e4d115e460a164447d5374bb5f2412
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-09 17:27:35 -0700

    initial migration file

--- data/migrations/20200108195042_initial.js
+++ data/migrations/20200108195042_initial.js
@@ -1,8 +1,52 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("recipes", table => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.integer("calories");
+    table.string("diet_type");
+  });
 
-exports.up = function(knex) {
-  
+  await knex.schema.createTable("steps", table => {
+    table.increments("id");
+    table.integer("step_number").notNullable();
+    table.string("instructions").notNullable();
+    table
+      .integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+
+  await knex.schema.createTable("ingredients", table => {
+    table.increments("id");
+    table.integer("name").notNullable();
+  });
+
+  await knex.schema.createTable("recipes_ingredients", table => {
+    table
+      .integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.integer("quantity");
+    table
+      .integer("ingredient_id")
+      .notNullable()
+      .references("id")
+      .inTable("ingredients")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.primary(["recipe_id", "ingredient_id"]);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("recipes_ingredients");
+  await knex.schema.dropTableIfExists("ingredients");
+  await knex.schema.dropTableIfExists("steps");
+  await knex.schema.dropTableIfExists("recipes");
 };
